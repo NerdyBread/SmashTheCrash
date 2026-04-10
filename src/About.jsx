e55a22e07ec8dd8a71b03b60817dae72df4ca453
NerdyBread42 <samfondacaro2006@gmail.com>
2026-04-10 08:24:48 -0400

    Update about page and temp PFP

--- src/About.jsx
+++ src/About.jsx
@@ -11,6 +11,16 @@ export default function About() {
                 to make the area safe for birds.
             </p>
             <p className='offwhite'>
+                Birds crash into our windows because they do not recognize glass as a solid material. As a result, the features that make Rochester so beautiful,
+                like its abundance of trees, often reflect off windows and trick them into thinking they are flying toward a place to land. In other cases, birds mistake raised walkways and
+                other transparent passages for clear skies, flying into them at breakneck speeds. These errors are usually fatal, but death is rarely swift; more often, they suffer traumatic
+                brain injuries, only to later succumb to their wounds.
+            </p>
+            <p className='offwhite'>
+                This is an urgent matter. In North America, populations of birds have already declined by almost a third since the 1970s. If we want to prevent their further loss,
+                we need to end bird-window collisions now. We need to smash the crash.
+            </p>
+            <p className='offwhite'>
                 Launched in September 2024, we are led by a team of student and faculty researchers and practitioners from RIT and the University of
                 Rochester in partnership with local non-profit organizations dedicated to environmental conservation. Through scientific research, art/design, 
                 and educational outreach, we are working to make our city a safer place for resident and migratory birds.
--- src/Home.jsx
+++ src/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
           <div className='w-50 ms-5 mt-5'>
             <h2 className='offwhite mb-4'>Over ten thousand birds are estimated to die from window collisions in Rochester, New York, every year.</h2>
             <p className='offwhite large'>Tragically, many of these birds collide into our buildings every year, resident and migratory species alike. Most of them are killed on impact, and many survivors die later from internal injuries.</p>
-            <p className='offwhite large'>Smash the Crash is a university-community initiative to protect the <span className='bold'>147</span> species of birds found on the RIT campus and the  <span className='bold'>364</span> spotted in Rochester as a whole.</p>
+            <p className='offwhite large'>Smash the Crash is a university-community initiative to protect the <span className='bold'>147</span> species of birds found on the RIT campus and the  <span className='bold'>364</span> spotted in Rochester as a whole by spreading awareness and eventually advocating for bird-friendly policy changes.</p>
             <p className='offwhite large'>In 2025, Dr. Richard Fadok, a professor of Anthropology at RIT, and the founder of Smash the Crash, created a Vertically Integrated Project course as a part of the initiative. The idea was to allow students to apply skills learned at RIT to this meaningful environmental initiative.</p>
             <p className='offwhite large'>This website is one of the projects, and all of the excellent work by other students this semester can be found below.</p>
           </div>
