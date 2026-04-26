b04a29c4ee721c686d06fee2e3d1396840d8e783
NerdyBread42 <samfondacaro2006@gmail.com>
2026-04-26 18:34:01 -0400

    Updated about and report text

--- src/About.jsx
+++ src/About.jsx
@@ -17,53 +17,53 @@ export default function About() {
                 <div className='big-text-container'>
                     <h1 className='heading mt-5 mb-5 pt-5'>About the Initiative</h1>
                     <p>
-                        Over ten thousand birds are estimated to die from window collisions in Rochester, New York, every year.
-                        Smash the Crash is a university-community initiative to study this problem and advocate for effective and evidence-driven solutions.
-                        Our ultimate goal is to enact bird-friendly RIT policy and eventually have legislation passed in the city of Rochester
-                        to make the area safe for birds.
+                        Every year in Rochester, New York, over ten thousand birds are estimated to die of window collisions. Smash the Crash is a
+                        university-community initiative to study this preventable problem and advocate for effective, evidence-driven solutions.
+                        Launched in September 2024, we are led by a team of student and faculty researchers at RIT and the University of Rochester
+                        and partner with local non-profit organizations dedicated to environmental conservation. Through scientific research,
+                        art/design, and educational outreach, we aim to pass bird-friendly legislation that will make our campuses and city safer for birds.
                     </p>
+                    <h2 className='heading mb-5 pt-5'>Why Do Birds Crash into Windows?</h2>
                     <p>
-                        Birds crash into our windows because they do not recognize glass as a solid material. As a result, the features that make Rochester so beautiful,
-                        like its abundance of trees, often reflect off windows and trick them into thinking they are flying toward a place to land. In other cases, birds mistake raised walkways and
-                        other transparent passages for clear skies, flying into them at breakneck speeds. These errors are usually fatal, but death is rarely swift; more often, they suffer traumatic
-                        brain injuries, only to later succumb to their wounds.
+                        Birds do not recognize glass as a solid material. As a result, the features that make Rochester so beautiful, like its abundance of
+                        trees, often reflect off windows and trick them into thinking they are flying toward a place to land. In other cases, birds mistake
+                        raised walkways and other transparent passages for clear skies, flying into them at breakneck speeds. These errors are usually fatal.
                     </p>
                     <p>
                         This is an urgent matter. In North America, populations of birds have already declined by almost a third since the 1970s. If we want to prevent their further loss,
-                        we need to end bird-window collisions now. We need to <span className='bold'>Smash the Crash</span>.
-                    </p>
-                    <p>
-                        Launched in September 2024, we are led by a team of student and faculty researchers and practitioners from RIT and the University of
-                        Rochester in partnership with local non-profit organizations dedicated to environmental conservation. Through scientific research, art/design, 
-                        and educational outreach, we are working to make our city a safer place for resident and migratory birds.
+                        we need to end bird-window collisions now. We need to <span className='bold'>smash the crash</span>.
                     </p>
                     <h2 className='heading mb-5 pt-5'>Why Do Birds Migrate?</h2>
                     <p>
-                        Birds follow flyways that have been passed down for generations in a fascinating show of ancestral animal mobility.
-                        Ultimately, they seek simple goals, similar to those that we would wish for our own families. In the winter, they fly south,
-                        seeking a warm, safe place with abundant food to wait out the harsh cold. In the spring, birds once again begin migration, using the Atlantic flyway, which passes through New York, to travel to nesting
-                        grounds that are uncrowded and ripe with food from the emerging summer weather. Here, they can truly resume their lives, settling down and caring for their offspring.
+                        In a fascinating show of ancestral animal mobility, birds follow migratory flyways that have been passed down for generations,
+                        including the Atlantic flyway, which passes through Rochester.
+                        In the winter, they fly south, seeking warmth, safety, and food while they wait out the cold. In the spring, birds retrace their steps,
+                        traveling to nesting grounds that are with abundant resources. Here, they can they breed and care for their young. Theirs are simple goals,
+                        not unlike what we would wish for our own families.
                     </p>
                     <p>
-                        Many birds stop over in Rochester to refuel as they finish their journey to the nesting grounds of Canada. Their plight of collisions, then,
-                        is especially tragic in this city, because they are so painfully close. Their journey is cut short just before they reach safety, by something
-                        that we are able to prevent.
+                        Every year tens of millions of birds stopover in Rochester to refuel as they up and down the Atlantic Flyway. Every year, their journey is cut short just before
+                        they can reach their destinations. This makes the plight of collisions in Rochester especially tragic. Thankfully, this is something that we are able to prevent.
                     </p>
                     <h2 className='heading mb-5 pt-5'>Birds Matter</h2>
                     <p>
-                        Many find it hard to commit themselves to the cause of preventing window strikes when we face ecological and environmental issues like climate change,
-                        deforestation, and ocean acidification. While bird-window collisions are a quieter, less covered environmental tragedy, this slow violence towards our
-                        avian friends is indeed a tragedy in its own right. One reason that ending bird-window collisions matters is that birds are a key contributor to the ecosystem of
-                        the entire country. They ensure equilibrium by eating around 400 million tons of insects annually, and they pollinate plants and spread seeds while feeding.
-                        Birds and their habitats make their surroundings more vibrant and beautiful. Birdwatching also is a massive driver of the economy: it generates an
-                        estimated $280 billion dollars of economic output annually.
+                        Many find it hard to commit to the cause of preventing window strikes when we face multiple, global environmental crises, such as climate change, deforestation,
+                        and ocean acidification. While bird-window collisions are a quieter, less covered environmental tragedy, this form of “slow violence” towards our avian friends
+                        is indeed a catastrophe in its own right. 
+                    </p>
+                    <p>
+                        One reason that ending bird-window collisions matters is that birds are an important contributor to the ecosystem of the entire country. They ensure equilibrium
+                        by eating around 400 million tons of insects annually, and they pollinate plants and spread seeds while feeding. Birds and their habitats make their surroundings
+                        more vibrant and beautiful. Birdwatching is also a massive driver of the economy; it generates an estimated $280 billion dollars of economic output annually.
                     </p>
                     <p>
                         Birds are a beautiful, precious animal, and they are dying. A primary driver of this sharp increase of avian mortality is bird-window collisions.
-                        In the United States, an estimated one billion birds die every year from these fatal window strikes. National legislation has failed to make it through
-                        Congress year after year, but on a smaller scale, universities and municipalities around the country have begun to recognize this profound ecological
-                        crisis and pass bird-friendly building policy. This often included requirements for new construction to be completed with bird-friendly materials for
-                        at-risk windows. Retrofitting existing buildings is also crucial, but thus far less common because of the cost.
+                        In the United States, an estimated one billion birds die every year from these fatal window strikes.
+                    </p>
+                    <p>
+                        While national legislation has failed to make it through Congress year after year, universities and municipalities around the country have begun to recognize
+                        this profound environmental problem and pass bird-friendly building policy. This often includes requirements for outfitting new buildings and retrofits with
+                        “bird-safe” measures that prevent collisions with existing structures.
                     </p>
                     <p>You can view our policy brief for RIT [here]</p>
                 </div>
--- src/Report.jsx
+++ src/Report.jsx
@@ -19,19 +19,21 @@ export default function Report() {
             <div className='d-flex justify-content-center mt-4'>
               <div className='big-text-container'>
                 <p className='mt-3'>
-                  If you see a collision, find a bird that you believe was a victim of a one, or find other evidence of a collision, please report it below with our tool.
-                  This data is a powerful tool that can work in conjunction with bird-safe design to protect local wildlife. Althought the eventual goal is to have only bird-safe
-                  buildings, it will give us a better picture of which specific ones are the most dangerous.
-                  The data gathered will also elucidate the scale of the problem and, eventually, allow us to wield empirical evidence in our advocacy for bird-friendly policy.
+                  Have you seen or heard a collision?
+                </p>
+                <p>
+                  If you've witnessed a collision or found a bird that you believe was a victim of one, please report it with our tool. The data you share with us
+                  will help us better protect our local wildlife, giving us a better picture of which buildings pose them the greatest threat and why we need to act now.
                 </p>
                 <div className='d-flex justify-content-center mb-5 mt-5'>
                   <a className='customButton nav-link' href='https://arcg.is/1HG5yD' target='_blank'>Go To Collision Reporting Survey</a>
                 </div>
                 <h5 className='weird-accent mb-3'>
-                  If you find a bird that has hit a window, or is otherwise injured but still alive, please follow the steps below to catch it and bring it to a wildlife rehabilitation center, in order to give it a better chance of survival.
+                  If you find a bird that has hit a window, or is otherwise injured but still alive, please follow the steps below to catch it and bring it to a wildlife rehabilitation center.
                 </h5>
                 <p>
-                  Many birds that collide with windows will appear stunned or confused but appear to otherwise be unharmed. Sadly, research has shown that many birds who initially survive collisions die later from internal injuries.
+                  Many birds that collide with windows will appear stunned or confused but appear to otherwise be unharmed. Sadly, research has shown that many birds who initially survive
+                  collisions die later from internal injuries. Your actions now can give the bird a chance of survival.
                 </p>
                 <ol>
                   <li>
@@ -39,11 +41,11 @@ export default function Report() {
                     You should also find a tissue, so that the bird has something soft to grasp in order to anchor itself. Place this at the bottom of the bag.
                   </li>
                    <li>
-                    If the disoriented bird is flying close to a building, it is best to let it settle before attempting to capture it. (Hi Dr. Fadok, thanks for proofreading this. I think the verb "capture" could use some work, but a better word isn't coming to mind)
+                    If the disoriented bird is flying close to a building, it is best to let it settle before attempting to help it.
                   </li>
                   <li>
-                    Slowly and deliberately approach the bird from behind. Make sure to position your hands so that the bird cannot fly away, but do not apply pressure to the bird. Small birds especially are incredibly fragile,
-                    so do not panic if they flutter or attempt to fly away, as closing your hands around the bird or squeezing it will further injure it.
+                    Slowly and deliberately approach the bird from behind. Make sure to position your hands so that the bird cannot fly away, but do not apply pressure to the bird. Birds are fragile,
+                    especially smaller ones, so do not panic if they flutter or attempt to fly away; if you close your hands around the bird or squeeze it, you risk injuring it further.
                   </li>
                   <li>
                     Place the bird gently into the container. Avoid interacting with it any more than necessary, and never give it food or water.
@@ -69,6 +71,10 @@ export default function Report() {
                     Contact your local wildlife rehabilitation center, who will likely ask you to bring it in. If you are unable to transport the bird, please call anyway to see what options are available.
                   </li>
                 </ol>
+                <p>
+                  From the City of Rochester website: For injured wildlife, call 311 and an Animal Services Officer will respond and transport the animal to a veterinarian or licensed wildlife rehabilitator for treatment or humane euthanasia.
+                  Do not put yourself at risk of being bitten. If there is concern regarding a bite, call us to pick up the injured animal.
+                </p>
               </div>
             </div>
         </div>
